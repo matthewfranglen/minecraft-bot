0c5742a57b0243b10e6974ec5e6206b529b008dc
Matthew Franglen <matthew@franglen.org>
2016-01-26 19:20:37 +0000

    More work on the simple commands

--- app/bot/simple.js
+++ app/bot/simple.js
@@ -1,3 +1,4 @@
+/* jshint esnext: true */
 
 exports.install = function (bot) {
   bot.simple = {
@@ -13,13 +14,40 @@ exports.install = function (bot) {
     moveTo: function (x, y, z, callback) {
       return moveTo(bot, x, y, z, callback);
     },
+    canDigAt: function (x, y, z) {
+      return canDigAt(bot, x, y, z);
+    },
     digAt: function (x, y, z, callback) {
       return digAt(bot, x, y, z, callback);
     },
     digAll: function (blocks, callback) {
       digAll(bot, blocks, callback);
+    },
+    tossAll: function (callback) {
+      tossAll(bot, callback);
     }
   };
+
+  bot.on('chat', function (username, message) {
+    if (message === 'come') {
+      bot.navigate.to(bot.players[username].entity.position);
+    }
+    else if (message === 'toss') {
+      tossAll(bot, function () {});
+    }
+  });
+
+  var checkEquipment = function () {
+    if (bot.targetDigBlock && ! bot.entity.heldItem) {
+      console.log('I am stopping digging');
+      bot.stopDigging();
+    }
+  };
+
+  bot.on('entityEquipmentChange', function (entity) {
+    console.log('Someone has changed their equipment');
+    checkEquipment();
+  });
 };
 
 pointAt = function (bot, x, y, z) {
@@ -34,6 +62,15 @@ blockAt = function (bot, x, y, z) {
   return bot.blockAt(pointAt(bot, x, y, z));
 };
 
+canDigAt = function (bot, x, y, z) {
+  if (! bot.entity.heldItem) {
+    return false;
+  }
+  var block = blockAt(bot, x, y, z);
+
+  return block.canHarvest(bot.entity.heldItem.type) && bot.canDigBlock(block);
+};
+
 moveTo = function (bot, x, y, z, callback) {
   if (callback) {
     bot.navigate.once('arrived', callback);
@@ -42,7 +79,12 @@ moveTo = function (bot, x, y, z, callback) {
 };
 
 digAt = function (bot, x, y, z, callback) {
-  bot.dig(blockAt(bot, x, y, z), callback);
+  if (canDigAt(bot, x, y, z)) {
+    bot.dig(blockAt(bot, x, y, z), callback);
+  }
+  else {
+    callback();
+  }
 };
 
 // blocks = [[1, 2, 0], [1, 2, 1], [1, 1, 0], [1, 1, 1], [1, 0, 0], [1, 0, 1]];
@@ -59,6 +101,24 @@ digAllLoop = function (bot, remaining, callback) {
       digAllLoop(bot, remaining, callback);
     };
     digAt(bot, current[0], current[1], current[2], digAgain);
+  }
+  else {
+    callback();
+  }
+};
+
+tossAll = function (bot, callback) {
+  remaining = bot.inventory.slots.filter(v => v);
+  tossAllLoop(bot, remaining, callback);
+};
+
+tossAllLoop = function (bot, remaining, callback) {
+  current = remaining.pop();
+  if (current) {
+    var loop = function () {
+      tossAllLoop(bot, remaining, callback);
+    };
+    bot.tossStack(current, loop);
   }
   else {
     callback();
