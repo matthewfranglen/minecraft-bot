d01182b3d983c728263d9317ae5e82f2569b3d33
Matthew Franglen <matthew@franglen.org>
2016-02-02 23:32:03 +0000

    Work on toss command

--- app/bot/inventory.js
+++ app/bot/inventory.js
@@ -2,27 +2,28 @@
 
 exports.tossAll = function () {
   var bot = this;
+  var items = bot.inventory.slots.filter(v => v);
+
+  if (! items) {
+    return Promise.reject('no items to toss');
+  }
 
   var tossOne = function (items) {
     var item = items.pop();
     if (! item) {
-      return;
+      return Promise.resolve();
     }
+
     return tossStack(bot, item).then(function () {
       tossOne(items);
     });
   };
 
-  return tossOne(bot.inventory.slots.filter(v => v));
+  return tossOne(items);
 };
 
 var tossStack = function (bot, item) {
   return new Promise(function (resolve, reject) {
-    if (! item) {
-      reject('missing');
-      return;
-    }
-
     bot.tossStack(item, function () {
       resolve('success');
     });
