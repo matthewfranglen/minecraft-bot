35b809ac16ac97358d78676f6a616fc7828f5298
Matthew Franglen <matthew@franglen.org>
2016-02-03 23:04:29 +0000

    Drop failing test

--- test/index.js
+++ test/index.js
@@ -2,7 +2,4 @@ import assert from 'assert';
 import minecraftBot from '../lib';
 
 describe('minecraft-bot', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
 });
