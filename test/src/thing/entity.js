f2ad5cc041053714f722d687c798236db00d99e8
Matthew Franglen <matthew@franglen.org>
2016-02-03 23:04:18 +0000

    Get rid of console message

--- test/src/thing/entity.js
+++ test/src/thing/entity.js
@@ -5,7 +5,6 @@ import entity from '../../../lib/src/thing/entity';
 
 describe('minecraft-bot thing entity', function () {
   it('should have the MUSHROOM_COW entity', function () {
-    console.log(entity);
     assert(entity.MUSHROOM_COW, 'check MUSHROOM_COW entity is created');
   });
 });
